133ec9f02f427e178b4a099dc68bb6d35fd14340
Zekiel Buzzeo <zeke.buzzeo@gmail.com>
2019-01-04 15:41:41 -1000

    final

--- secret_number.js
+++ secret_number.js
@@ -3,8 +3,6 @@ module.exports = function( ) {
     let secretNumber = ( Math.random( ) * 100000 ) + 1;
     function secretNumberGenerator ( ) {
         return secretNumber;
-
     }
     return secretNumberGenerator;
-
 };
